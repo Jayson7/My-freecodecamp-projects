670bf07d0998fdb0d8e30a936c41c3a85407b2f3
Jayson7 <lexxiijoo70@gmail.com>
2020-04-09 02:20:57 +0100

    javascript

--- Timestamp/server.js
+++ Timestamp/server.js
@@ -1,27 +1,12 @@
-var express = require('express');
-var app = express();
-var strftime = require('strftime');
+const express = require("express");
+const app = express();
+const parseTime = require("./app/parseTime.js");
 
-app.get('', function (req, res) {
-  res.send("Timestamp microservice.");
-})
+app.use("/", express.static("./public"));
 
-app.get('/:date', function (req, res) {
-  var num = Number(req.params.date);
-  if (isNaN(num)) {
-    var time = Date.parse(req.params.date);
-  }
-  else{
-    var time = new Date(num*1000);
-  }
-  if (isNaN(time)) {
-    res.send({'unix':null,'natural':null});
-  }
-  else{
-    res.send({'unix':time/1000,'natural':strftime('%B %d, %Y', new Date(time))});
-  }
+app.get("/:time", function (req, res) {
+    const output = parseTime(req.params.time);
+    res.send(JSON.stringify(output));
 });
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(process.env.PORT || 8080);
